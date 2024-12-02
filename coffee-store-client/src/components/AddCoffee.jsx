d9994e22a40ee719170d614a8435bf7eab03b63f
Amit Kumar Sutradhar <dev.amitkumarsutradhar@gmail.com>
2024-12-02 11:20:13 +0600

    Dot Env configuration

--- coffee-store-client/src/components/AddCoffee.jsx
+++ coffee-store-client/src/components/AddCoffee.jsx
@@ -1,7 +1,27 @@
 const AddCoffee = () => {
     return (
         <div>
-            
+            <h2>Add a Coffee</h2>
+            <form action="">
+                <div className="flex">
+                    <div className="">
+                        <label className="form-control ">
+                            <div className="label">
+                                <span className="label-text">Coffee name</span>
+                            </div>
+                            <input type="text" placeholder="Coffee name" className="input input-bordered w-full max-w-xs" />
+                        </label>
+                    </div>
+                    <div className="">
+                        <label className="form-control ">
+                            <div className="label">
+                                <span className="label-text">Available Quantity</span>
+                            </div>
+                            <input type="text" placeholder="Available Quantity" className="input input-bordered w-full max-w-xs" />
+                        </label>
+                    </div>
+                </div>
+            </form>
         </div>
     );
 };
