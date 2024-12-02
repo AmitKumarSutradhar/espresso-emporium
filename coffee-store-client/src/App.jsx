2366979665765f1009a517c77dd42b668c87a4b3
Amit Kumar Sutradhar <dev.amitkumarsutradhar@gmail.com>
2024-12-02 18:43:40 +0600

    Show all coffee and delete coffee

--- coffee-store-client/src/App.jsx
+++ coffee-store-client/src/App.jsx
@@ -1,11 +1,21 @@
 import { useState } from 'react'
 import './App.css'
+import { useLoaderData } from 'react-router-dom'
+import CoffeeCard from './components/CoffeeCard';
 
 function App() {
 
+  const coffees = useLoaderData();
+
   return (
     <>
-      <h1>Vite + React</h1>
+      <h1>Coffees : {coffees.length}</h1>
+
+      <div className="grid md:grid-cols-2 gap-10">
+        {
+          coffees.map(coffee => <CoffeeCard key={ coffee._id } coffee={coffee}></CoffeeCard>)
+        }
+      </div>
     </>
   )
 }
---
+++ coffee-store-client/src/components/CoffeeCard.jsx
@@ -0,0 +1,81 @@
+import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
+
+const CoffeeCard = ({ coffee }) => {
+
+    const { _id, name, quantity, supplier, taste, category, details, photo } = coffee;
+
+    const handleDelete = (id) => {
+        console.log(id);
+
+
+        const swalWithBootstrapButtons = Swal.mixin({
+            customClass: {
+                confirmButton: "btn btn-success",
+                cancelButton: "btn btn-danger"
+            },
+            buttonsStyling: false
+        });
+        swalWithBootstrapButtons.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonText: "Yes, delete it!",
+            cancelButtonText: "No, cancel!",
+            reverseButtons: true
+        }).then((result) => {
+            if (result.isConfirmed) {
+                fetch(`http://localhost:5000/coffee/${_id}`, {
+                    method: 'DELETE'
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log(data);
+                        if (data.deletedCount > 0) {
+                            swalWithBootstrapButtons.fire({
+                                title: "Deleted!",
+                                text: "Your coffee item has been deleted.",
+                                icon: "success"
+                            });
+                        }
+                    })
+
+            } else if (
+                /* Read more about handling dismissals below */
+                result.dismiss === Swal.DismissReason.cancel
+            ) {
+                swalWithBootstrapButtons.fire({
+                    title: "Cancelled",
+                    text: "Your imaginary file is safe :)",
+                    icon: "error"
+                });
+            }
+        });
+    }
+
+    return (
+        <div className="card card-side bg-base-100 shadow-xl">
+            <figure>
+                <img
+                    src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
+                    alt="Movie" />
+            </figure>
+            <div className="card-body flex-row justify-between p-5">
+                <div className="text-left">
+                    <h2 className="card-title">{name}</h2>
+                    <p>Click the button to watch on Jetflix app.</p>
+                </div>
+                <div className="">
+                    <div className="join join-vertical space-y-4">
+                        <button className="btn join-item">View</button>
+                        <Link to={`updateCoffee/${_id}`}> <button className="btn join-item">Edit</button> </Link>
+                        <button onClick={() => handleDelete(_id)} className="btn bg-red-600 join-item">X</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default CoffeeCard;
